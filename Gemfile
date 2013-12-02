b1ceee658175153727963ad47a492599667248f8
Nica <nicanorperera@gmail.com>
2013-12-02 15:31:41 -0300

    Sqlite3, ActiveRecord and Json

--- Gemfile
+++ Gemfile
@@ -1,9 +1,14 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
+gem 'sqlite3'
+# gem 'pg'
+gem 'activerecord'
+gem 'sinatra-activerecord'
 
 group :development do
   gem 'rake'
+  gem 'tux'
 end
 
 group :test do
--- app.rb
+++ app.rb
@@ -1,6 +1,21 @@
 require 'bundler'
+require "sinatra"
+require "sinatra/activerecord"
+
+ENV['RACK_ENV'] = 'development'
 Bundler.require :default, ENV['RACK_ENV'].to_sym
+
+class Resource < ActiveRecord::Base
+  validates :name, presence: true
+end
 
 get '/' do
   'Hello World'
+end
+
+get '/resources' do
+  content_type :json
+  @resources = Resource.all
+
+  @resources.to_json
 end
